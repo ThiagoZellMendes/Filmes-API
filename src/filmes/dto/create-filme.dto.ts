d5ad8b61a1b299b2b2c79b300451dfc219e49287
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2022-08-15 22:49:06 -0300

    Feat: ConfigurationInitialOfTypeORM

--- src/filmes/dto/create-filme.dto.ts
+++ src/filmes/dto/create-filme.dto.ts
@@ -1,1 +1,34 @@
-export class CreateFilmeDto {}
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateFilmeDto {
+  @IsString()
+  @IsNotEmpty()
+  Titulo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  Cover: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  anoLancamento: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  generos: string;
+
+  @IsString()
+  @IsNotEmpty()
+  mediaImbd: string;
+}
+
+// Titulo
+// Capa
+// Cover
+// Ano de lançamento
+// Generos
+// Média IMDB
--- src/filmes/entities/filme.entity.ts
+++ src/filmes/entities/filme.entity.ts
@@ -1,1 +1,66 @@
-export class Filme {}
+// Titulo
+// Capa
+// Cover
+// Ano de lançamento
+// Generos
+// Média IMDB
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Filme extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @UpdateDateColumn({
+    name: 'update_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAT: Date;
+
+  @Column({
+    name: 'description',
+    type: 'varchar',
+    nullable: false,
+    length: 255,
+  })
+  description: string;
+
+  @Column({
+    name: 'cover',
+    type: 'varchar',
+    nullable: false,
+    length: 255,
+  })
+  cover: string;
+
+  @Column({
+    name: 'ano_lancamento',
+    type: 'date',
+    nullable: false,
+    length: 4,
+  })
+  anoLancamento: number;
+
+  @Column({
+    name: 'generos',
+    type: 'varchar',
+    nullable: false,
+    length: 255,
+  })
+  generos: string;
+
+  @Column({
+    name: 'media_imdb',
+    type: 'number',
+    nullable: false,
+    length: 2,
+  })
+  mediaImdb: number;
+}
