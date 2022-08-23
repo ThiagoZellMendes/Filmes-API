6db5adf7639c3f62b1f913c0cd839b70cbf6ae44
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2022-08-23 17:27:07 -0300

    Feat: ApplyRolesUserInControllers

--- src/filmes/filmes.controller.ts
+++ src/filmes/filmes.controller.ts
@@ -7,10 +7,14 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { RoleUsers } from 'src/user/entities/role.enum';
 import { CreateFilmeDto } from './dto/create-filme.dto';
 import { UpdateFilmeDto } from './dto/update-filme.dto';
 import { Filme } from './entities/filme.entity';
@@ -21,6 +25,8 @@ export class FilmesController {
   constructor(private readonly filmesService: FilmesService) {}
 
   @Post()
+  @Roles(RoleUsers.ADMIN)
+  @UseGuards(RolesGuard)
   @ApiTags('Filmes')
   create(@Body() createFilmeDto: CreateFilmeDto) {
     return this.filmesService.create(createFilmeDto);
@@ -28,6 +34,7 @@ export class FilmesController {
 
   @Get()
   @IsPublic()
+  @UseGuards(RolesGuard)
   @ApiTags('Filmes')
   @ApiQuery({ name: 'page', required: false, type: Number })
   @ApiQuery({ name: 'limit', required: false, type: Number })
@@ -41,18 +48,23 @@ export class FilmesController {
 
   @Get(':id')
   @IsPublic()
+  @UseGuards(RolesGuard)
   @ApiTags('Filmes')
   findOneBy(@Param('id') id: string) {
     return this.filmesService.findOneBy(id);
   }
 
   @Patch(':id')
+  @Roles(RoleUsers.ADMIN)
+  @UseGuards(RolesGuard)
   @ApiTags('Filmes')
   update(@Param('id') id: string, @Body() updateFilmeDto: UpdateFilmeDto) {
     return this.filmesService.update(id, updateFilmeDto);
   }
 
   @Delete(':id')
+  @Roles(RoleUsers.ADMIN)
+  @UseGuards(RolesGuard)
   @ApiTags('Filmes')
   remove(@Param('id') id: string) {
     return this.filmesService.remove(id);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,48 +1,59 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
 } from '@nestjs/common';
-import { UserService } from './user.service';
+import { ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { IsPublic } from 'src/auth/decorators/is-public.decorator';
-import { ApiTags } from '@nestjs/swagger';
+import { RoleUsers } from './entities/role.enum';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @Roles(RoleUsers.ADMIN)
+  @UseGuards(RolesGuard)
   @ApiTags('User')
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.userService.create(createUserDto);
   }
 
   @Get()
-  @IsPublic()
+  @Roles(RoleUsers.USER, RoleUsers.ADMIN)
+  @UseGuards(RolesGuard)
   @ApiTags('User')
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':email')
+  @Roles(RoleUsers.USER, RoleUsers.ADMIN)
   @ApiTags('User')
   findByEmail(@Body('email') email: string) {
     return this.userService.findByEmail(email);
   }
 
   @Patch(':id')
+  @Roles(RoleUsers.ADMIN)
+  @UseGuards(RolesGuard)
   @ApiTags('User')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @Roles(RoleUsers.ADMIN)
+  @UseGuards(RolesGuard)
   @ApiTags('User')
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
