8d38f8ba7ed507eb4485813c571f21504f500367
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2022-08-23 13:34:41 -0300

    Chore: RemoveImportsNeverUsedAndMore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FilmesModule } from './filmes/filmes.module';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
-import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt.auth.guard';
-import * as dotenv from 'dotenv';
+import { RolesGuard } from './auth/guards/roles.guard';
+import { FilmesModule } from './filmes/filmes.module';
+import { UserModule } from './user/user.module';
 
 dotenv.config();
 
@@ -29,15 +29,14 @@ dotenv.config();
     }),
     UserModule,
     AuthModule,
+    FilmesModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: process.env.IS_EXPIRATION_IN },
     }),
-    FilmesModule,
   ],
   controllers: [AppController],
   providers: [
-    AuthService,
     AppService,
     {
       provide: APP_GUARD,
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
